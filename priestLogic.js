708fee83a8e4522ee83913307aae4ae109e2374b
andyghoffman <52428392+andyghoffman@users.noreply.github.com>
2020-05-27 12:05:46 -0400

    added range check for healing

--- priestLogic.js
+++ priestLogic.js
@@ -14,11 +14,21 @@ function priestAuto(target)
 			if(character.hp < character.max_hp * healThreshold && !character.rip)
 				damagedPartyMembers++;
 
-			if(partyMember.hp < (partyMember.max_hp * healThreshold) && !partyMember.rip && is_in_range(partyMember, "heal"))
+			if(partyMember.hp < (partyMember.max_hp * healThreshold) && !partyMember.rip)
 			{
+				if(!is_in_range(partyMember, "heal"))
+				{
+					smart_move(
+						character.x + (partyMember.x - character.x) * 0.3,
+						character.y + (partyMember.y - character.y) * 0.3
+					);
+					
+					return;
+				}
+
 				damagedPartyMembers++;
 				
-				if(damagedPartyMembers > 1 && character.mp >= G.skills.partyheal.mp && !is_on_cooldown("partyheal"))
+				if(damagedPartyMembers > 1 && character.mp >= G.skills.partyheal.mp && !is_on_cooldown("partyheal") && !is_on_cooldown("heal"))
 				{
 					use_skill("partyheal");
                     reduce_cooldown("partyheal", character.ping);
@@ -27,7 +37,7 @@ function priestAuto(target)
                     					
 					return;
 				}
-				else if(!is_on_cooldown("heal") && character.mp >= G.skills.heal.mp)
+				else if(!is_on_cooldown("heal") && !is_on_cooldown("partyheal") && character.mp >= G.skills.heal.mp)
 				{
                     log("Priest is healing " + partyMember.name);
 
@@ -46,14 +56,14 @@ function priestAuto(target)
 		}
     });
     
-    if(character.hp < (character.max_hp * healThreshold) && !is_on_cooldown("heal"))
+    if(character.hp < (character.max_hp * healThreshold) && !is_on_cooldown("heal") && !is_on_cooldown("partyheal"))
 	{
         heal(character);
         game_log("Priest is healing herself.");
 		return;
     }
 	
-	if(character.mp >= G.skills.partyheal.mp && !is_on_cooldown("curse") && !target.s.curse && target.hp > target.max_hp*0.5)
+	if(character.mp >= G.skills.curse.mp && !is_on_cooldown("curse") && !target.s.curse && target.hp > target.max_hp*0.5)
 	{
 		use_skill("curse");
 		reduce_cooldown("curse", character.ping);
