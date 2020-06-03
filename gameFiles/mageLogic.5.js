81bef3605521d933633338cef58d96676e0fabab
andyghoffman <52428392+andyghoffman@users.noreply.github.com>
2020-06-03 14:04:56 -0400

    adding lins to game files

---
+++ gameFiles/mageLogic.5.js
@@ -0,0 +1,10 @@
+function load_file(fileName) {
+    const fs = require('fs')
+    const data = fs.readFileSync(fileName, 'utf8')
+    return data;
+}
+
+var library=document.createElement("script");
+    library.type="text/javascript";
+    library.text=load_file("C:/GitHub/lotusAdventureBot/mageLogic.js");
+    document.getElementsByTagName("head")[0].appendChild(library);
