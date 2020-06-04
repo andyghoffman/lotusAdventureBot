e6c487b84b7ce152c4cca2d8af75113a7b5f837d
andyghoffman <52428392+andyghoffman@users.noreply.github.com>
2020-06-03 22:04:30 -0400

    new file paths

--- gameFiles/priestLogic.4.js
+++ gameFiles/priestLogic.4.js
@@ -1,10 +1,11 @@
-function load_file(fileName) {
+function load_file(fileName)
+{
     const fs = require('fs')
     const data = fs.readFileSync(fileName, 'utf8')
     return data;
 }
 
-var library=document.createElement("script");
-    library.type="text/javascript";
-    library.text=load_file("C:/GitHub/lotusAdventureBot/priestLogic.js");
-    document.getElementsByTagName("head")[0].appendChild(library);
+var library = document.createElement("script");
+library.type = "text/javascript";
+library.text = load_file("C:/GitHub/lotusAdventureBot/code/priestLogic.js");
+document.getElementsByTagName("head")[0].appendChild(library);
